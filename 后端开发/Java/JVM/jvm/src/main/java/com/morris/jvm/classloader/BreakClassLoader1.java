806b2017abcb0d7ae489bec5f51d4280be4c4cc5
chenlian <chenlianwj65651@touna.cn>
2018-11-21 11:28:48 +0800

    class loader

--- 后端开发/Java/JVM/jvm/src/main/java/com/morris/jvm/classloader/BreakClassLoader1.java
+++ 后端开发/Java/JVM/jvm/src/main/java/com/morris/jvm/classloader/BreakClassLoader1.java
@@ -3,7 +3,7 @@ package com.morris.jvm.classloader;
 public class BreakClassLoader1 {
     public static void main(String[] args) throws ClassNotFoundException {
         ClassLoader extClassLoader = BreakClassLoader1.class.getClassLoader().getParent();
-        MyClassLoader myClassLoader = new MyClassLoader(extClassLoader);
+        MyClassLoader myClassLoader = new MyClassLoader(extClassLoader);// 设置父类加载器为扩展类加载器
         Class<?> clazz = myClassLoader.loadClass("com.morris.jvm.classloader.HelloWorld");
         System.out.println(clazz.getClassLoader());
     }
--- 后端开发/Java/JVM/jvm/src/main/java/com/morris/jvm/classloader/BreakClassLoader2.java
+++ 后端开发/Java/JVM/jvm/src/main/java/com/morris/jvm/classloader/BreakClassLoader2.java
@@ -2,7 +2,7 @@ package com.morris.jvm.classloader;
 
 public class BreakClassLoader2 {
     public static void main(String[] args) throws ClassNotFoundException {
-        MyClassLoader myClassLoader = new MyClassLoader(null);
+        MyClassLoader myClassLoader = new MyClassLoader(null);// 设置父类加载器为null
         Class<?> clazz = myClassLoader.loadClass("com.morris.jvm.classloader.HelloWorld");
         System.out.println(clazz.getClassLoader());
     }
